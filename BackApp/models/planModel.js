010c179b73adfbea923368cd080ec6604c1f6344
Donovan700 <bryanrakotosedson@gmail.com>
2024-03-04 19:05:13 +0300

    fix: set/get IdplanModel

--- BackApp/models/planModel.js
+++ BackApp/models/planModel.js
@@ -26,7 +26,7 @@ class PlanModel {
 
     async getPlanById(idplan) {
         try {
-            const { rows } = await pool.query('SELECT * FROM plan WHERE idplan = ?', [idplan]);
+            const { rows } = await pool.query('SELECT * FROM plan WHERE idplan = $1', [idplan]);
             return rows[0];
         } catch (error) {
             throw error;
@@ -36,7 +36,7 @@ class PlanModel {
     async updatePlan(idplan, planData) {
         try {
             const { dateplan } = planData;
-            const { rowCount } = await pool.query('UPDATE plan SET dateplan = ? WHERE id = ?', [dateplan, idplan]);
+            const { rowCount } = await pool.query('UPDATE plan SET dateplan = $1 WHERE id = $2', [dateplan, idplan]);
             return rowCount > 0;
         } catch (error) {
             throw error;
@@ -45,7 +45,7 @@ class PlanModel {
 
     async deleteUser(idplan) {
         try {
-            const {rowCount} = await pool.query('DELETE FROM plan WHERE idplan = ?', idplan);
+            const {rowCount} = await pool.query('DELETE FROM plan WHERE idplan = $1', idplan);
             return rowCount > 0;
         }
         catch (error) {
