fa8bd6d1efe0c28a62dfab7a0b52c5e191d37d67
Donovan700 <bryanrakotosedson@gmail.com>
2024-03-05 10:37:05 +0300

    add: Front-end page

--- BackApp/app.js
+++ BackApp/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = 6969;
 
 const routes = require('./routes');
+
+app.use(express.static(path.join(__dirname, '../../PerfectApp')));
 
 app.use(bodyParser.json());
 
